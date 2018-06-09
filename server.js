53282e3b257f8901afaaed13028e3b6d4cd7014e
Chris Broski <chrisbroski@gmail.com>
2018-06-09 13:48:17 -0400

    renamed files

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const http = require('http');
 const fs = require('fs');
 const WebSocketServer = require('websocket').server;
 var server;
+var socketServer;
 
 function webServer(req, rsp) {
     rsp.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    fs.createReadStream(__dirname + '/index.html').pipe(rsp);
+    fs.createReadStream(__dirname + '/client.html').pipe(rsp);
 }
 
 server = http.createServer(webServer);
@@ -14,7 +15,7 @@ server.listen(6503, function() {
     console.log((new Date()) + " Server is listening on port 6503");
 });
 
-var socketServer = new WebSocketServer({
+socketServer = new WebSocketServer({
     httpServer: server,
     autoAcceptConnections: false
 });
