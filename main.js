c0080f45f93c8257eeab0342b278a9f50b305f35
Chris Broski <chrisbroski@gmail.com>
2018-06-09 13:07:10 -0400

    serve html with server

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-var http = require('http');
-var WebSocketServer = require('websocket').server;
-var server = http.createServer();
+const http = require('http');
+const fs = require('fs');
+const WebSocketServer = require('websocket').server;
+var server;
+
+function webServer(req, rsp) {
+    rsp.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    fs.createReadStream(__dirname + '/index.html').pipe(rsp);
+}
+
+server = http.createServer(webServer);
 
 server.listen(6503, function() {
     console.log((new Date()) + " Server is listening on port 6503");
 });
 
-var wsServer = new WebSocketServer({
+var socketServer = new WebSocketServer({
     httpServer: server,
     autoAcceptConnections: false
 });
 
-wsServer.on('connect', function(connection) {
+socketServer.on('connect', function(connection) {
     console.log("Connected to client at " + connection.remoteAddress);
 });
 
-wsServer.on('request', function (request) {
+socketServer.on('request', function (request) {
     var connection = request.accept("", request.origin);
 
     // Send a message to the client
